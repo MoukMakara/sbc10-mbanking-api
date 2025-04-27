f0e31fdc0fbdff93778594c5bd771b06ccc3bf9a
Mouk Makara <makaramouk@gmail.com>
2025-04-27 16:29:13 +0700

    add OpenAPIDefinition

--- src/main/java/co/istad/mbanking/Sbc10MbankingApiApplication.java
+++ src/main/java/co/istad/mbanking/Sbc10MbankingApiApplication.java
@@ -1,9 +1,12 @@
 package co.istad.mbanking;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Mobile Banking API",description = "API documentation for the Mobile Banking application",version = "1.0"))
 public class Sbc10MbankingApiApplication {
 
     public static void main(String[] args) {
